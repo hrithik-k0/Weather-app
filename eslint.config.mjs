f640c7c652b607c2dbe1026d4ba79549237cbc4b
hrithik-k0 <hrithikk179@gmail.com>
2025-12-07 11:49:28 +0530

    mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,6 +3,11 @@ import js from "@eslint/js";
 export default [
   js.configs.recommended,
   {
+    languageOptions: {
+      globals: {
+        ...js.environments.browser.globals, // include browser globals
+      }
+    },
     rules: {
       "no-unused-vars": "warn",
       "no-console": "off"
